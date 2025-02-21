1834dc4cc93a4a0619de7ccb220070767a9c2d43
vtannn <134799875+vtannn@users.noreply.github.com>
2025-02-21 22:55:12 +0700

    Update defs.h

--- defs.h
+++ defs.h
@@ -3,5 +3,8 @@
 const int SCREEN_WIDTH=800;
 const int SCREEN_HEIGHT=600;
 const char* WINDOW_TITLE="Vtan";
+bool quit=false;
+#define BOARD_X 0
+#define BOARD_Y 0
+#define CELLSIZE 30
 #endif // DEFS_H
-

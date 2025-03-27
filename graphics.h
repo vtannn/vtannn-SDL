3ff9d273cfbe0e89d2cdd34fe924ab84aa618f95
vtannn <134799875+vtannn@users.noreply.github.com>
2025-03-28 01:24:38 +0700

    Update graphics.h

--- graphics.h
+++ graphics.h
@@ -1,12 +1,53 @@
 #ifndef GRAPHICS_H
 #define GRAPHICS_H
-#include "logic.h"
 #include "defs.h"
+#include<algorithm>
+#include<vector>
+using namespace std;
+struct Pipe
+{
+    SDL_Texture* texture;
+    int x=SCREEN_WIDTH;
+    int height;
+    void update()
+    {
+        if(x>=-300) x-=game_speed;
+    }
+};
+struct ScrollingBackground
+{
+    SDL_Texture *texture;
+    int scrollingOffset=0;
+    int width,height;
+
+    void setTexture(SDL_Texture* _texture)
+    {
+        texture=_texture;
+        SDL_QueryTexture(texture,NULL,NULL,&width,&height);
+    }
+
+    void scroll()
+    {
+        scrollingOffset-=game_speed;
+        if(scrollingOffset<0) scrollingOffset=width;
+    }
+};
+struct Sprite
+{
+    SDL_Texture *texture;
+    vector<SDL_Texture*>clips;
+    int currentframes=0;
+    void tick()
+    {
+        currentframes=(currentframes+1)%clips.size();
+        texture=clips[currentframes];
+    }
+};
 struct Graphics
 {
     SDL_Window* window;
     SDL_Renderer* renderer;
-    SDL_Texture* cell_Empty,*cell_X,*cell_O,*winner1,*winner2;
+    SDL_Texture* bird1,*bird2,*bird3;
     void logErrorAndExit(const char* msg,const char* error)
     {
         SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION,SDL_LOG_PRIORITY_ERROR,"%s : %s",msg,error);
@@ -25,15 +66,6 @@ struct Graphics
         SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY,"linear");
         SDL_RenderSetLogicalSize(renderer,SCREEN_WIDTH,SCREEN_HEIGHT);
     }
-    void init()
-    {
-        initSDL();
-        cell_Empty=loadTexture("xo/cell_empty.png");
-        cell_X=loadTexture("xo/cell_x.png");
-        cell_O=loadTexture("xo/cell_o.png");
-        winner1=loadTexture("xo/winner-1.png");
-        winner2=loadTexture("xo/winner-2.png");
-    }
     void prepareScene(SDL_Texture* background)
     {
         SDL_RenderClear(renderer);
@@ -50,64 +82,86 @@ struct Graphics
         if(texture==nullptr) logErrorAndExit("CreateTexture",IMG_GetError());
         return texture;
      }
+     void init()
+     {
+        initSDL();
+        bird1=loadTexture("fbimg/bird1.PNG");
+        bird2=loadTexture("fbimg/bird2.PNG");
+        bird3=loadTexture("fbimg/bird3.PNG");
+     }
      void renderTexture(SDL_Texture* texture,int x,int y)
      {
          SDL_Rect dst;
          dst.x=x;
          dst.y=y;
          SDL_QueryTexture(texture,NULL,NULL,&dst.w,&dst.h);
-         dst.w=dst.w*30/100;
-         dst.h=dst.h*30/100;
          SDL_RenderCopy(renderer,texture,NULL,&dst);
+     }
+     void renderPipe(Pipe pipe,int y,int position)
+     {
+         SDL_Rect dst;
+         dst.x=pipe.x;
+         dst.y=y+pipe_gap*position-(position==1 ? pipe.height : 0 );
+         SDL_QueryTexture(pipe.texture,NULL,NULL,&dst.w,&dst.h);
+         dst.h+=pipe.height*position-pipe_gap;
+         if(position==1) SDL_RenderCopy(renderer,pipe.texture,NULL,&dst);
+         else
+         {
+             SDL_RenderCopyEx(renderer,pipe.texture,NULL,&dst,0,NULL,SDL_FLIP_VERTICAL);
+         }
+     }
+     void renderTextureAngle(SDL_Texture* texture,int x,int y,int angle)
+     {
+         SDL_Rect dst;
+         dst.x=x;
+         dst.y=y;
+         SDL_QueryTexture(texture,NULL,NULL,&dst.w,&dst.h);
+         SDL_RenderCopyEx(renderer,texture,NULL,&dst,angle,NULL,SDL_FLIP_NONE);
      }
      void QUIT()
      {
          SDL_DestroyWindow(window);
          SDL_DestroyRenderer(renderer);
-         SDL_DestroyTexture(cell_Empty);
-         SDL_DestroyTexture(cell_X);
-         SDL_DestroyTexture(cell_O);
          IMG_Quit();
          SDL_Quit();
      }
-     void prepareBackground(const Tictactoe &game)
+     void renderBIRD(int x,int y,Sprite &sprite,int angle)
      {
-         for(int i=0;i<BOARDSIZE;i++)
-         {
-             for(int j=0;j<BOARDSIZE;j++)
-             {
-                 int x=BOARD_X + i*CELLSIZE;
-                 int y=BOARD_Y + j*CELLSIZE;
-                 renderTexture(cell_Empty,x,y);
-            }
-         }
-         presentRenderer();
+         renderTextureAngle(sprite.texture,x,y,angle);
      }
-     void render(const Tictactoe& game,int i,int j)
+     void renderBackground(ScrollingBackground bgr,int x)
      {
-             int x=BOARD_X + i*CELLSIZE;
-             int y=BOARD_Y + j*CELLSIZE;
-             switch(game.board[i][j])
-             {
-                 case EMPTY_CELL: renderTexture(cell_Empty,x,y);break;
-                 case X_CELL: renderTexture(cell_X,x,y);break;
-                 case O_CELL: renderTexture(cell_O,x,y);break;
-             }
-         presentRenderer();
-     }
-     void stopGame(Tictactoe &game,int row,int col)
-     {
-         bool kt=game.check();
-         if(kt)
-         {
-             int x=BOARD_X + row*CELLSIZE;
-             int y=BOARD_Y + col*CELLSIZE;
-             if(kt==1) prepareScene(winner1);
-             else prepareScene(winner2);
-             presentRenderer();
-             SDL_Delay(1000);
-             quit=true;
-         }
+         renderTexture(bgr.texture,bgr.scrollingOffset,x);
+         renderTexture(bgr.texture,bgr.scrollingOffset-bgr.width,x);
      }
 };
+vector<Pipe>Pipes;
+bool ss(Pipe x,Pipe y)
+{
+    return x.x>y.x;
+}
+void Pipes_delete()
+{
+    sort(Pipes.begin(),Pipes.end(),ss);
+    while(!Pipes.empty()&&Pipes[Pipes.size()-1].x<-100)
+    {
+        Pipes.pop_back();
+    }
+}
+void Pipes_update()
+{
+    for(int i=0;i<Pipes.size();i++)
+    {
+        Pipes[i].update();
+    }
+}
+void Pipes_draw(Graphics graphics)
+{
+    for(int i=0;i<Pipes.size();i++)
+    {
+        Pipe pipe=Pipes[i];
+        graphics.renderPipe(pipe,170,1);
+        graphics.renderPipe(pipe,0,-1);
+    }
+}
 #endif // GRAPHICS_H

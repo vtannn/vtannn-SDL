9bf0cd65cf54d558f73db8a69508a2537e2f9b8e
vtannn <134799875+vtannn@users.noreply.github.com>
2025-03-28 01:24:26 +0700

    Update defs.h

--- defs.h
+++ defs.h
@@ -1,19 +1,13 @@
 #ifndef DEFS_H
 #define DEFS_H
-#define INITIAL_SPEED 10
 const int SCREEN_WIDTH=700;
 const int SCREEN_HEIGHT=800;
 const char* WINDOW_TITLE="Vtan";
 bool quit=false;
 bool flying=false;
 bool game_over=false;
-#define BOARD_X 0
-#define BOARD_Y 0
-#define CELLSIZE 30
-const int BIRD_CLIPS[][4] = {
-    {0,0,51,36},
-    {0,0,51,36},
-    {0,0,51,36}
-};
-#define BIRD_FRAMES 3
+int pipe_gap=150;
+int game_speed=5;
+Uint32 pipe_frequency=3000;
+SDL_Texture *pipe;
 #endif // DEFS_H

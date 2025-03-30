1ef77bafd2c8874a4e79fb196e65d5c289bb26a3
vtannn <134799875+vtannn@users.noreply.github.com>
2025-03-30 20:51:06 +0700

    Done score and high score board

--- defs.h
+++ defs.h
@@ -1,5 +1,7 @@
 #ifndef DEFS_H
 #define DEFS_H
+#include<string>
+using namespace std;
 const int SCREEN_WIDTH=700;
 const int SCREEN_HEIGHT=800;
 const char* WINDOW_TITLE="Vtan";
@@ -8,6 +10,10 @@ bool flying=false;
 bool game_over=false;
 int pipe_gap=150;
 int game_speed=5;
+int score=0;
+int high_score=0;
 Uint32 pipe_frequency=3000;
+Uint32 time_now;
 SDL_Texture *pipe;
+char *score_text;
 #endif // DEFS_H
